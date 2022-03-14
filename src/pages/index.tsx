b124ddf5ff9ac9af861a44369200c7aea1d96587
Eduardo Prudêncio <eduardo.prudencio@bizvox.local>
2022-03-14 11:22:51 -0300

    Estilizando a home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
 
 export default function Home() {
   return  (
@@ -6,9 +7,17 @@ export default function Home() {
       <Head>
         <title>Home</title>
       </Head>
-      <h3>
-        Hello <span>World</span>
-      </h3>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
