014e87c12580985e81e3ba84658aec1ead51e392
Eduardo Prudêncio <eduardo.prudencio@bizvox.local>
2022-03-15 18:47:33 -0300

    Integrando com o fauna DB

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import {signIn, signOut, useSession} from 'next-auth/react' 
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,8 +6,20 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+  const {data: session} = useSession()
+  
+  function heandleSubscribe(){
+  
+    if(!session){
+      signIn();
+      return;
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button type="button" className={styles.subscribeButton}
+    onClick={()=> heandleSubscribe()}
+    >
       Subscribe Now
     </button>
   )
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna"
+import {query as q} from 'faunadb'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +12,35 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try{
+          await fauna.query(
+            q.If(
+              q.Not(
+                q.Exists(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(email)
+                  )
+                )
+              ),
+              q.Create(
+                q.Collection('users'),
+                {data: {email}}
+              ),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            )
+          )
+      }catch{
+        return false
+      }
+    }
+}
+
 })
